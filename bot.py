e7744cf1320693ffecb1a8a181861eaedba6bea8
Hasnain khan <161922960+hasnainkk-07@users.noreply.github.com>
2024-07-15 20:39:54 +0530

    create by @hasnainkk-07

---
+++ bot.py
@@ -0,0 +1,50 @@
+import html
+import random
+from telegram import Update, Bot
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from config import TELEGRAM_TOKEN, OWNER_ID
+
+def start(update: Update, context: CallbackContext):
+    user = update.effective_user
+    mention = f"<a href='tg://user?id={user.id}'>{html.escape(user.first_name)}</a>"
+    update.message.reply_html(f'Hello! I am a bot that deletes edited messages except those from the owner. Welcome, {mention}!')
+
+def check_edit(update: Update, context: CallbackContext):
+    bot: Bot = context.bot
+
+    # Check if the update is an edited message
+    if update.edited_message:
+        edited_message = update.edited_message
+        
+        # Get the chat ID and message ID
+        chat_id = edited_message.chat_id
+        message_id = edited_message.message_id
+        
+        # Get the user who edited the message
+        user_id = edited_message.from_user.id
+        
+        # Create the mention for the user
+        user_mention = f"<a href='tg://user?id={user_id}'>{html.escape(edited_message.from_user.first_name)}</a>"
+        
+        # Delete the message if the editor is not the owner
+        if user_id != OWNER_ID:
+            bot.delete_message(chat_id=chat_id, message_id=message_id)
+            
+            # Send a message notifying about the deletion
+            bot.send_message(chat_id=chat_id, text=f"{user_mention} just edited a message. I deleted their original message.", parse_mode='HTML')
+
+def main():
+    updater = Updater(TELEGRAM_TOKEN, use_context=True)
+    dp = updater.dispatcher
+
+    # Start command handler
+    dp.add_handler(CommandHandler("start", start))
+    
+    # Message edit handler
+    dp.add_handler(MessageHandler(Filters.update.edited_message, check_edit))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
