3d8f801f12fad548c066fdd489578a3041774581
Hasnain khan <161922960+hasnainkk-07@users.noreply.github.com>
2024-07-20 20:35:21 +0530

    create by @hasnainkk-07

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-TELEGRAM_TOKEN = 'ADD YOUR TELEGRAM BOT TOKEN'
-OWNER_ID = 'ADD YOUR TELEGRAM ID'
+TELEGRAM_TOKEN = 'ADD YOUR TELEGRAM BOT TOKEN' # Add Your Telegram Bot Token from @BotFather
+OWNER_ID = 'ADD YOUR TELEGRAM ID' # Add Here your telegram id go @Raiden_Robot in pm type /id copy and paste here
