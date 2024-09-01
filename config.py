144995818e16696c74bd3730711e013d3f66254f
Hasnain khan <161922960+hasnainkk-07@users.noreply.github.com>
2024-09-01 19:37:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,28 @@
-TELEGRAM_TOKEN = 'ADD YOUR TELEGRAM BOT TOKEN' # Add Your Telegram Bot Token from @BotFather
-OWNER_ID = 'ADD YOUR TELEGRAM ID' # Add Here your telegram id go @Raiden_Robot in pm type /id copy and paste here
+#TELEGRAM_TOKEN = 'ADD YOUR TELEGRAM BOT TOKEN' # Add Your Telegram Bot Token from @BotFather
+#OWNER_ID = 'ADD YOUR TELEGRAM ID' # Add Here your telegram id go @Raiden_Robot in pm type /id copy and paste here
+
+
+
+# Telegram bot token
+TELEGRAM_TOKEN = "7243757952:AAGS0U7oZ7AJcg0KdQ04KgXWSuNu_Uu8lQg"
+
+# Your telegram user ID (owner)
+OWNER_ID = 6346273488
+
+SUDO_ID = [6346273488, 5907205317, 1805959544, 1284920298, 5881613383, 6864672519, 5458968679]
+
+MONGO_URI = "mongodb+srv://herobh123456:hasnainkk07@hasnainkk07.uqjekii.mongodb.net/?retryWrites=true&w=majority"
+
+DB_NAME = "Toji_Fushiguro"
+
+DB_URI = "postgres://jrjcokem:VlG00PuZj0IgXZfc5dKrP2njj1DIHIWM@isabelle.db.elephantsql.com/jrjcokem"
+
+LOGGER = True
+
+BOT_NAME = "Edit Guardian"
+
+SUPPORT_ID = (-1002105665930)
+
+API_ID = 24683098
+
+API_HASH = "e4055cd239464e50e69bd73057c05dd3"
