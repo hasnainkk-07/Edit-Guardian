e86e14f92259d8b47948e5b5481295d51da1a457
Hasnain khan <161922960+hasnainkk-07@users.noreply.github.com>
2024-09-01 19:39:36 +0530

    Create hasnainkk07.py

---
+++ hasnainkk-07/hasnainkk07.py
@@ -0,0 +1,46 @@
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
+
+PM_START_TEXT = """
+*Hello* {}[✨]({}) 👋 I'm your 𝗘𝗱𝗶𝘁 𝗚𝘂𝗮𝗿𝗱𝗶𝗮𝗻 𝗕𝗼𝘁, here to maintain a secure environment for our discussions.
+
+🚫 𝗘𝗱𝗶𝘁𝗲𝗱 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 𝗗𝗲𝗹𝗲𝘁𝗶𝗼𝗻: 𝗜'𝗹𝗹 𝗿𝗲𝗺𝗼𝘃𝗲 𝗲𝗱𝗶𝘁𝗲𝗱 𝗺𝗲𝘀𝘀𝗮𝗴𝗲𝘀 𝘁𝗼 𝗺𝗮𝗶𝗻𝘁𝗮𝗶𝗻 𝘁𝗿𝗮𝗻𝘀𝗽𝗮𝗿𝗲𝗻𝗰𝘆.
+
+📣 𝗡𝗼𝘁𝗶𝗳𝗶𝗰𝗮𝘁𝗶𝗼𝗻𝘀: 𝗬𝗼𝘂'𝗹𝗹 𝗯𝗲 𝗶𝗻𝗳𝗼𝗿𝗺𝗲𝗱 𝗲𝗮𝗰𝘁𝗶𝗺𝗲 𝘁𝗶𝗺𝗲 𝗮 𝗺𝗲𝘀𝘀𝗮𝗴𝗲 𝗶𝘀 𝗱𝗲𝗹𝗲𝘁𝗲𝗱.
+
+🌟 𝗚𝗲𝘁 𝗦𝘁𝗮𝗿𝘁𝗲𝗱:
+1. Add me to your group.
+2. I'll start protecting instantly.
+
+➡️ Click on 𝗔𝗱𝗱 𝗠𝗲 𝗧𝗼 𝗬𝗼𝘂𝗿 𝗚𝗿𝗼𝘂𝗽 to add me and keep our group safe!
+"""
+    
+buttons = [
+    [
+        InlineKeyboardButton(
+            text="𝗔𝗱𝗱 𝗠𝗲 𝗧𝗼 𝗬𝗼𝘂𝗿 𝗚𝗿𝗼𝘂𝗽",
+            url=f"https://t.me/EditGuardian_Bot?startgroup=true",
+        ),
+    ],
+    [
+        InlineKeyboardButton(text="𝗦𝘂𝗽𝗽𝗼𝗿𝘁", url=f"https://t.me/Raiden_Support"),
+        InlineKeyboardButton(text="𝗦𝗼𝘂𝗿𝗰𝗲", url=f"https://github.com/hasnainkk-07/Edit-Guardian"),
+    ],    
+    [
+        InlineKeyboardButton(text="𝗢𝘄𝗻𝗲𝗿", url=f"https://t.me/hasnainkk"),
+    ],
+    
+]
+
+IMG = [
+"https://telegra.ph/file/73c9aa7b5e1a2e053d915.jpg",
+"https://telegra.ph/file/6cf4d7a5d07cdbc5c4c4f.jpg",
+"https://telegra.ph/file/3938993e7f83b9201d961.jpg",
+"https://telegra.ph/file/867bd553810ac3a4cf09f.jpg",
+"https://telegra.ph/file/d102719ef028b224e0842.jpg",
+"https://telegra.ph/file/63dbc9108dca4a91121af.jpg",
+"https://telegra.ph/file/5225ee47a9cbb9a0e85b1.jpg",
+"https://telegra.ph/file/ee9751a286fd983f08086.jpg",
+"https://telegra.ph/file/fbfa4262e467652e75d83.jpg",
+"https://telegra.ph/file/865ce3676d535ec83dce9.jpg",
+]
+PM_START_IMG = "https://te.legra.ph/file/821cfbf567b7227b26fab.jpg"
